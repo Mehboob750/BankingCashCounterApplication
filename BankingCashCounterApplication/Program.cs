25f9bf9e2ee71fb67a8116a07383f350953f2cff
Mehboob750 <ms568203@gmail.com>
2020-06-03 12:47:08 +0530

    Welcome to Banking Cash Counter Application

---
+++ BankingCashCounterApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingCashCounterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Banking Cash Counter");
+        }
+    }
+}
