d7a8fd60128d19987538c7251e434f9e18346d86
Mehboob750 <ms568203@gmail.com>
2020-06-04 06:42:40 +0530

    Added Empty Linked List

---
+++ BankingCashCounterApplication/LinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace BankingCashCounterApplication
+{
+    //LinkedList DataStructure created to store and Manipulate the Customer Data
+    public class LinkedList
+    {
+        Node head;
+        internal static bool checkCustomer;
+
+        public class Node
+        {
+            public String name;
+            public double cash;
+            public Node next;
+
+            public Node
+                 (string name, double cash)
+            {
+                this.name = name;
+                this.cash = cash;
+            }
+        }
+
+        //To check the LinkedList is Empty or Not
+        public bool isEmpty()
+        {
+            if (head == null)
+                return true;
+            return false;
+        }
+
+    }
+}
--- BankingCashCounterApplication/Program.cs
+++ BankingCashCounterApplication/Program.cs
@@ -7,6 +7,8 @@ namespace BankingCashCounterApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Banking Cash Counter");
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine(linkedList.isEmpty());
         }
     }
 }
